0862945905c8908d2276b001613d45fdffe819d4
snehakumari-37 <92073117+snehakumari-37@users.noreply.github.com>
2021-10-14 11:43:52 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,11 @@
+function doesFileExist(urlToFile) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('HEAD', urlToFile, false);
+    xhr.send();
+     
+    if (xhr.status == "404") {
+        return false;
+    } else {
+        return true;
+    }
+}
